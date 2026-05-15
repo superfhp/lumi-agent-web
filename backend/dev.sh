eaff0a65f0dd674e70c815c4e36828c6d178209f
superfhp <hank_fu@163.com>
2026-05-15 10:28:06 +0800

    1234

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,3 @@
-export CORS_ALLOW_ORIGIN="http://localhost:5173;http://localhost:8080"
-PORT="${PORT:-8080}"
+export CORS_ALLOW_ORIGIN="http://47.99.95.132:5173;http://47.99.95.132:8888"
+PORT="${PORT:-8888}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips "${FORWARDED_ALLOW_IPS:-*}" --reload
